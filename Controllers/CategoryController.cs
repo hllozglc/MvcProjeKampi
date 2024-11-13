8c8ce26be3a2e281425c1a006adf9fd4177e4c4d
hllozglc <hllozglc@hotmail.com>
2024-11-13 06:04:35 +0300

    26

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcProjeKampi.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager();
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetAllBl();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public IActionResult AddCategory() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            cm.CategoryAddBl(category);
+            return RedirectToAction("GetCategoryList");
         }
     }
 }
